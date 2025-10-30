61c27cd65b6c674d1bcbb6f45736c96066817563
Matt_p <mattportmann1@gmail.com>
2025-10-30 16:24:05 -0600

    Save_Game_Board

--- src/game-odjects/game-board.ts
+++ src/game-odjects/game-board.ts
@@ -1,15 +1,40 @@
-import { BOARD_COLOR } from "../constants";
+import { BOARD_COLOR, BRICK_SIZE } from "../constants";
+import { Brick } from "./brick";
 
-export class GmaeBoard {
+export class GameBoard {
     color: string = BOARD_COLOR;
     rows: number = 8;
     cols:number = 8;
-    constructor(private readonly ctx: CanvasRenderingContext2D,
-        private readonly x: number,
-        private readonly Y: number,
-    ){}
+    private readonly x: number;
+    private cells: Array<Brick> = [];
+    constructor(
+        private readonly ctx: CanvasRenderingContext2D,
+        x: number,
+        private readonly y: number,
+    ){
+        let width = this.rows * BRICK_SIZE;
+        //let Height = this.cols * BRICK_SIZE;
+        this.x = x - (width / 2);
+        this.initGrid();
+    }
+
+    private initGrid() {
+        const {rows, cols, x, y, ctx}= this;
+        for(let row = 0; row < rows; row++){
+            for(let col = 0; col < cols; col++){
+                let bx = x + BRICK_SIZE * col;
+                let by = y + BRICK_SIZE * row;
+                let cell = new Brick(ctx, bx, by, BOARD_COLOR);
+                this.cells.push(cell);
+            }
+
+        }
+
+    }
 
         public draw(): void{
-
+            this.cells.forEach(c => {
+                c.draw();
+            })
         }
     }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { canvas, ctx, initCanvas } from './canvas-ctx';
-import { Brick } from './game-odjects/brick';
+import { GameBoard } from './game-odjects/game-board';
 import './style.css'
 
 initCanvas();
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //ctx.fillStyle = "red";
+  //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  let brick = new Brick(ctx, canvas.width/2, canvas.height/2);
-  brick.draw();
+  //let brick = new Brick(ctx, canvas.width/2, canvas.height/2);
+  //brick.draw();
+
+  let gb = new GameBoard(ctx, canvas.width / 2, 150);
+  gb.draw();
