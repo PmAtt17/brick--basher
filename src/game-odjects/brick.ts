139f218c639421d8a64ad1a589f60da447d77ba8
Matt_p <mattportmann1@gmail.com>
2025-11-19 16:23:58 -0700

    Challenge Day

--- src/game-odjects/brick.ts
+++ src/game-odjects/brick.ts
@@ -1,4 +1,4 @@
-import { BRICK_SIZE } from "../constants";
+import { BRICK_SIZE, HIGHLIGHTCOLOR } from "../constants";
 import type { Point } from "./point";
 
  export class Brick{
@@ -7,15 +7,17 @@ import type { Point } from "./point";
     
     
 
-    constructor(private readonly ctx: CanvasRenderingContext2D,
-
+    constructor(
+        private readonly ctx: CanvasRenderingContext2D,
         public x: number,
         public y: number,
-        public readonly color: string = "blue"){
+        public readonly color: string = "blue",
+        public highlightColor: string = "HIGHLIGHTCOLOR"
+    ){
      this.x = x;
      this.y = y;
      this.ctx = ctx;
-    
+
     }
 
     public draw(): void {
@@ -85,6 +87,19 @@ import type { Point } from "./point";
         return isInPath;
     }
 
+    public center() {
+        let x = (this.x + BRICK_SIZE/2);
+        let y = (this.y + BRICK_SIZE/2);
+        
+        let centerPoint : number = (x, y);
+        
+    }
     
+    public isOtherOver(point: Point) : boolean{
+        const path = new Path2D();
+        path.rect(this.x + this.size/2, this.y + this.size/2, 1, 1);
 
+        const isOnCenter = this.ctx.isPointInPath(path,point.x, point.y);
+        return isOnCenter;
+    }
 }
